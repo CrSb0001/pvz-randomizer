44193c5b5bba21165fb5effea80cda5cd3d420a8
CrSb0001 <127436709+CrSb0001@users.noreply.github.com>
2025-06-24 20:03:10 -0500

    Update util_helpers.py

--- util_helpers.py
+++ util_helpers.py
@@ -29,3 +29,20 @@ def range_(
 
 def convert_to_level(lvl: int) -> str:
 	return f'{lvl // 10 + (lvl % 10 > 0)}-{(lvl % 10) or 10}'
+
+def at_least(n: int, gen: list[int]) -> bool:
+    s = 0
+    for x in gen:
+        s += x
+        if s >= n:
+            return True
+    
+    return False
+
+def num_of_instas_by_level(insta_levels, idx: int) -> int:
+    n = 0
+    iterator = iter(insta_levels)
+    while next(iterator, 60) < idx:
+        n += 1
+    
+    return n
